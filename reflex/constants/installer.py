d71e03ae8bc855fd2c22dce06497c1c6ed9ed427
Joe Wright <joejoejoew@gmail.com>
2025-09-19 16:48:42 +0000

    hardcode frontend_path 404 page generated path

--- reflex/constants/installer.py
+++ reflex/constants/installer.py
@@ -106,7 +106,7 @@ class PackageJson(SimpleNamespace):
 
         DEV = "react-router dev --host"
         EXPORT = "react-router build"
-        PROD = "sirv ./build/client --single 404.html --host"
+        PROD = "sirv ./build/client --dev --single ./pas-lab/404.html --host"
 
     PATH = "package.json"
 
