0f88c802d43f3dafc2556dee80129d7b3cdbca67
benjol <benjol@amazon.com>
2025-09-21 21:57:40 -0700

    is_prod_mode should be fine for prerender_routes during compilation

--- reflex/app.py
+++ reflex/app.py
@@ -625,7 +625,7 @@ class App(MiddlewareMixin, LifespanMixin):
         """
         from reflex.vars.base import GLOBAL_CACHE
 
-        self._compile(prerender_routes=True)
+        self._compile(prerender_routes=is_prod_mode())
 
         config = get_config()
 
